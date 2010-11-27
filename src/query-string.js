08e31e8af7457416ab8f89f45dc87a2a64db62e0
Sven Dahlstrand <sven.dahlstrand@gmail.com>
2010-11-27 12:19:06 +0100

    Add missing semicolons.

--- src/query-string.js
+++ src/query-string.js
@@ -2,23 +2,23 @@ function QueryString() {
   this.raw = window.location.search;
 
   this.present = function() {
-    return (this.raw.length > 0)
-  }
+    return (this.raw.length > 0);
+  };
 
   this.includes = function(name) {
     re = '[?&]' + name;
 
     return (this.raw.search(re) != -1);
-  }
+  };
 
   this.set = function(name, value) {
     var keyValuePair = name + '=' + value;
     var queryString = this.present() ? (this.raw + '&') : '?';
 
     this.raw = queryString + keyValuePair;
-  }
+  };
 
   this.reload = function() {
     window.location.search = this.raw;
-  }
-};
+  };
+}
